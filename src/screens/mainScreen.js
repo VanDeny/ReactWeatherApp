c7b7cc4a19c29a4ffc7aa7bbfa1c3dc56c8d8197
VanDeny <badura.denis@gmail.com>
2020-05-12 08:08:50 +0200

    Initial commit

---
+++ src/screens/mainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+import drawer from "../components/drawer";
+
+const mainScreen = () => {
+    return <View>
+        <drawer />
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default mainScreen;
---
+++ src/screens/searchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const searchScreen = () => {
+    return <View>
+        <Text>test</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default searchScreen;
