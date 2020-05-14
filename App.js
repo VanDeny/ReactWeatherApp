4d755a3234af6b575df697f5324838a2b5cd2a02
VanDeny <badura.denis@gmail.com>
2020-05-15 00:39:29 +0200

    Fix hooks

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {Text} from 'react-native';
 import Geocoder from 'react-native-geocoding';
 import { createDrawerNavigator  } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+
+import {getCurrentLocation} from './src/utils/getCurrentLocation'
 
 import forecastScreen from './src/screens/forecastScreen'
 import listOfCitiesScreen from './src/screens/forecastScreen'
@@ -12,11 +14,28 @@ Geocoder.init('AIzaSyC5QtVetrDWZ89XJCV-kz40cC3bCwEKNro');
 
 const Drawer = createDrawerNavigator();
 
+
 export default function App() {
+    const [location, setLocation] = useState();
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            };
+            setLocation(location);
+        });
+    } else {
+        alert("Geolocation Unavaible!");
+    }
+
     return (
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen name="Forecast" component={forecastScreen} />
+                <Drawer.Screen name="Forecast"
+                               component={forecastScreen}
+                               {...this.location}/>
                 <Drawer.Screen name="List of Cities" component={listOfCitiesScreen} />
                 <Drawer.Screen name="Search" component={searchScreen} />
             </Drawer.Navigator>
--- src/screens/forecastScreen.js
+++ src/screens/forecastScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
-const forecastScreen = ({  }) => {
+const forecastScreen =  ({location}) => {
+    console.log({location});
+    const [temperature, setTemperature] = useState('0');
     return (
         <View>
             <Text>test</Text>
